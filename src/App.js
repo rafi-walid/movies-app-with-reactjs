7e874b580ff46dde45f2bd0459a762a2bd2ef919
walid <walid_rafi@hotmail.com>
2024-02-22 21:41:27 +0100

    change some code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Cards } from './Cards';
 
 
-const API_URL = ' http://www.omdbapi.com/?i=tt3896198&apikey=1b86fab1';
+const API_URL = ' https://www.omdbapi.com/?i=tt3896198&apikey=1b86fab1';
 const App = () => {
     const [movies,setMovies] = useState([]);
     const [searcht,setSearcht] = useState('');
